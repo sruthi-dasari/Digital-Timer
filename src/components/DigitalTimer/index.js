81b39518ec72d5d94ecae82a47106f0c2aa1b854
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2022-12-28 17:53:56 +0530

    unexpected string concatenation error

--- src/components/DigitalTimer/index.js
+++ src/components/DigitalTimer/index.js
@@ -2,46 +2,119 @@ import './index.css'
 import {Component} from 'react'
 
 class DigitalTimer extends Component {
-  constructor() {
-    state = {
-      timerStarted: false,
-    }
+  state = {
+    isTimerStarted: false,
+    minutes: 25,
   }
 
-    const playIcon =
-      'https://assets.ccbp.in/frontend/react-js/play-icon-img.png'
-    const pauseIcon =
-      'https://assets.ccbp.in/frontend/react-js/pause-icon-img.png'
-    const startPauseImgUrl = timerStarted ? playIcon : pauseIcon
+  onClickMinus = () => {
+    this.setState(prevState => ({
+      minutes: prevState.minutes - 1,
+    }))
+  }
+
+  onClickPlus = () => {
+    this.setState(prevState => ({
+      minutes: prevState.minutes + 1,
+    }))
+  }
+
+  onClickStart = () => {}
 
   render() {
-    
+    const {isTimerStarted, minutes} = this.state
+
+    const startPauseImgUrl = isTimerStarted
+      ? 'https://assets.ccbp.in/frontend/react-js/pause-icon-img.png'
+      : 'https://assets.ccbp.in/frontend/react-js/play-icon-img.png'
+
+    const startPauseAltText = isTimerStarted ? 'pause icon' : 'play icon'
+
+    const runningPausedText = isTimerStarted ? 'Running' : 'Paused'
+
+    const startPauseText = isTimerStarted ? 'Pause' : 'Start'
+
+    let authButton
+
+    if (isTimerStarted) {
+      authButton = (
+        <div className="start-pause-container">
+          <button
+            className="startPauseBtn"
+            type="button"
+            onClick={this.onClickPause}
+          >
+            <img
+              src={startPauseImgUrl}
+              className="start-pause-icon"
+              alt={startPauseAltText}
+            />
+          </button>
+          <h1 className="start-pause-text">{startPauseText}</h1>
+        </div>
+      )
+    } else {
+      authButton = (
+        <div className="start-pause-container">
+          <button
+            className="startPauseBtn"
+            type="button"
+            onClick={this.onClickStart}
+          >
+            <img
+              src={startPauseImgUrl}
+              className="start-pause-icon"
+              alt={startPauseAltText}
+            />
+          </button>
+          <h1 className="start-pause-text">{startPauseText}</h1>
+        </div>
+      )
+    }
+
+    const stringifiedMinutes = minutes > 9 ? minutes : '0' + minutes
+    const stringifiedSeconds = seconds > 9 ? seconds : '0' + seconds
 
     return (
       <div className="app-container">
         <h1 className="main-heading">Digital Timer</h1>
-        <div className="app-img">
+        <div className="app-img-container">
           <div className="main-timer">
-            <h1 className="timer-text"></h1>
-            <p className="start-pause-text"></p>
+            <h1 className="timer-text">{minutes}</h1>
+            <p className="running-paused-text">{runningPausedText}</p>
           </div>
         </div>
         <div className="start-pause-reset-container">
-          <div className="start-pause-container">
-            <img src={startPauseImgUrl} className="start-pause-icon" />
-            <h1 className="start-pause-text">{}</h1>
-          </div>
+          {authButton}
           <div className="reset-container">
-            <img src="https://assets.ccbp.in/frontend/react-js/reset-icon-img.png" className="reset-icon" />
-            <h1 className="reset-text">{}</h1>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/reset-icon-img.png"
+              className="reset-icon"
+              alt="reset icon"
+            />
+            <h1 className="reset-text">Reset</h1>
           </div>
         </div>
         <div className="timer-limit-container">
-          <p className="timer-limit-title"></p>
+          <p className="timer-limit-title">Set Timer Limit</p>
           <div className="timer-setting-container">
-            <button className="minus-btn">-</button>
-            <button className="time-btn">25</button>
-            <button className="plus-btn">+</button>
+            <button
+              className="operator-btn"
+              type="button"
+              onClick={this.onClickMinus}
+            >
+              -
+            </button>
+            <button className="time-btn" type="button">
+              {minutes}
+            </button>
+            <button
+              className="operator-btn"
+              type="button"
+              onClick={this.onClickPlus}
+            >
+              +
+            </button>
           </div>
         </div>
       </div>
